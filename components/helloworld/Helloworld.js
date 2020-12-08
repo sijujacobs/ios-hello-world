57de5fbe8d0bd3b462ccf6ea06fd148fb11f552e
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-12-08 16:21:03 -0500

    helloworld comp changes

---
+++ components/helloworld/Helloworld.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return <Text>Hello worlddddd!-----</Text>;
+  }
+}
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
+import HelloWorldApp from './components/helloworld/Helloworld'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HelloWorldApp);
